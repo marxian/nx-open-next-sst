67c64d54fc6ff8ccb61d1f04596e3bf10c5e4ee6
Rupert Redington <rupert@lola.tech>
2023-12-11 15:40:44 +0000

    add sst and open-nexts new --package-json flag

--- stacks/web.ts
+++ stacks/web.ts
@@ -3,7 +3,7 @@ import { NextjsSite, StackContext } from 'sst/constructs';
 export function web({ stack }: StackContext) {
   const site = new NextjsSite(stack, 'Site', {
     path: 'dist/apps/web',
-    buildCommand: 'exit 0',
+    buildCommand: 'exit 0'
   });
 
   stack.addOutputs({
